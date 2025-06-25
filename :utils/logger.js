61b06f84bdaa84b7881bd51faa3afb1307a75bab
JOHNJOHNFM <158992033+johnjohnfm@users.noreply.github.com>
2025-06-25 12:12:37 -0500

    Delete :utils directory

--- :utils/logger.js
+++
@@ -1,6 +0,0 @@
-// logger.js
-export function logValidation(input, result) {
-  console.log(`[${new Date().toISOString()}] Validation Run:`);
-  console.log("Input:", input);
-  console.log("Output:", result);
-}
